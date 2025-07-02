b3be74c97b95b62bd1b53aecf0c416df6ab07dd6
zancas <zancas@zingolabs.org>
2025-07-02 11:23:14 -0600

    apply version 8.4.0 AGP across the project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,7 @@ pluginManagement {
         mavenCentral()
     }
     plugins {
-        id("com.android.library") version "8.2.2"
+        id("com.android.library") version "8.4.0"
         id("org.jetbrains.kotlin.android") version "2.0.21"
     }
 }
