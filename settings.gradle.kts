b1f3690732248cea87371966598c3a8e97889c01
zancas <zancas@zingolabs.org>
2025-07-02 16:24:32 -0600

    add license and FAIL_ON_PROJECT_REPOS

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,7 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
